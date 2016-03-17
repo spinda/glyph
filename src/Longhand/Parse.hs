e22bad65055289cc3e737264c8f3eb90f2636993
Michael Smith <michael@spinda.net>
2016-03-16 19:55:26 -0700

    Generalize Scala type parsing code

--- src/Longhand/Parse.hs
+++ src/Longhand/Parse.hs
@@ -3,7 +3,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Longhand.Parse (
-    parseLetterForm
+    -- * Letter Form File Parsing
+    parseLetterFormFile
+  , parseLetterForm
   ) where
 
 import Control.Applicative
@@ -17,25 +19,31 @@ import Data.Attoparsec.Combinator
 
 import Longhand.Types
 
+--------------------------------------------------------------------------------
+-- Letter Form File Parsing ----------------------------------------------------
+--------------------------------------------------------------------------------
+
+parseLetterFormFile :: FilePath -> IO (Either String Letter)
+parseLetterFormFile = fmap parseLetterForm . B.readFile
+
 parseLetterForm :: ByteString -> Either String Letter
 parseLetterForm = parseOnly editingP
 
+--------------------------------------------------------------------------------
+-- Parse Muse-CGH Letter Text File Format --------------------------------------
+--------------------------------------------------------------------------------
 
 editingP :: Parser Letter
-editingP = do
-  string "Editing"
-  parens $ letterP <* comma <* listP (signed decimal)
+editingP = scalaTypeP "Editing" $ letterP <* comma <* listP (signed decimal)
 
 letterP :: Parser Letter
-letterP = do
-  string "Letter"
-  parens $ do
-    segments <- vectorP letterSegmentP <* comma
-    kind     <- letterKindP
-    return $ Letter
-      { letterKind     = kind
-      , letterSegments = segments
-      }
+letterP = scalaTypeP "Letter" $ do
+  segments <- vectorP letterSegmentP <* comma
+  kind     <- letterKindP
+  return $ Letter
+    { letterKind     = kind
+    , letterSegments = segments
+    }
 
 letterKindP :: Parser LetterKind
 letterKindP = (UpperCaseLetter <$ string "Uppercase")
@@ -43,56 +51,66 @@ letterKindP = (UpperCaseLetter <$ string "Uppercase")
           <|> (PunctuationMark <$ string "PunctuationMark")
 
 letterSegmentP :: Parser LetterSegment
-letterSegmentP = do
-  string "LetterSeg"
-  parens $ do
-    curve         <- cubicCurveP <* comma
-    startWidth    <- double      <* comma
-    endWidth      <- double      <* comma
-    alignTangent  <- boolP       <* comma
-    isStrokeBreak <- boolP
-    return $ LetterSegment
-      { letterSegmentCurve         = curve
-      , letterSegmentStartWidth    = startWidth
-      , letterSegmentEndWidth      = endWidth
-      , letterSegmentAlignTangent  = alignTangent
-      , letterSegmentIsStrokeBreak = isStrokeBreak
-      }
+letterSegmentP = scalaTypeP "LetterSeg" $ do
+  curve         <- cubicCurveP <* comma
+  startWidth    <- double      <* comma
+  endWidth      <- double      <* comma
+  alignTangent  <- boolP       <* comma
+  isStrokeBreak <- boolP
+  return $ LetterSegment
+    { letterSegmentCurve         = curve
+    , letterSegmentStartWidth    = startWidth
+    , letterSegmentEndWidth      = endWidth
+    , letterSegmentAlignTangent  = alignTangent
+    , letterSegmentIsStrokeBreak = isStrokeBreak
+    }
 
 cubicCurveP :: Parser CubicCurve
-cubicCurveP = do
-  string "CubicCurve"
-  parens $ do
-    param1 <- vec2P <* comma
-    param2 <- vec2P <* comma
-    param3 <- vec2P <* comma
-    param4 <- vec2P
-    return $ CubicCurve
-      { cubicCurveParam1 = param1
-      , cubicCurveParam2 = param2
-      , cubicCurveParam3 = param3
-      , cubicCurveParam4 = param4
-      }
+cubicCurveP = scalaTypeP "CubicCurve" $ do
+  param1 <- vec2P <* comma
+  param2 <- vec2P <* comma
+  param3 <- vec2P <* comma
+  param4 <- vec2P
+  return $ CubicCurve
+    { cubicCurveParam1 = param1
+    , cubicCurveParam2 = param2
+    , cubicCurveParam3 = param3
+    , cubicCurveParam4 = param4
+    }
 
 vec2P :: Parser (Double, Double)
-vec2P = do
-  string "Vec2"
-  parens $ do
-    x <- double <* comma
-    y <- double
-    return (x, y)
+vec2P = scalaTypeP "Vec2" $ do
+  x <- double <* comma
+  y <- double
+  return (x, y)
 
+--------------------------------------------------------------------------------
+-- Scala Type Parsers ----------------------------------------------------------
+--------------------------------------------------------------------------------
 
 boolP :: Parser Bool
 boolP = (True  <$ string "true")
     <|> (False <$ string "false")
 
 vectorP :: Parser a -> Parser [a]
-vectorP p = string "Vector" *> parens (sepBy p comma)
+vectorP = scalaListTypeP "Vector"
 
 listP :: Parser a -> Parser [a]
-listP p = string "List" *> parens (sepBy p comma)
+listP = scalaListTypeP "List"
 
+--------------------------------------------------------------------------------
+-- Scala Type Parsing Tools ----------------------------------------------------
+--------------------------------------------------------------------------------
+
+scalaTypeP :: ByteString -> Parser a -> Parser a
+scalaTypeP name args = string name *> parens args
+
+scalaListTypeP :: ByteString -> Parser a -> Parser [a]
+scalaListTypeP name item = scalaTypeP name $ sepBy item comma
+
+--------------------------------------------------------------------------------
+-- Misc Parsing Utilities ------------------------------------------------------
+--------------------------------------------------------------------------------
 
 between :: Parser a -> Parser b -> Parser c -> Parser c
 between start stop inner = start *> inner <* stop
