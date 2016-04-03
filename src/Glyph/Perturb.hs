80ccd5b622d69aaeee2cc1dc6cc94d219b4f4736
Michael Smith <michael@spinda.net>
2016-04-03 13:35:14 -0700

    Bi-license under LGPLv3 and MPL2

--- src/Glyph/Perturb.hs
+++ src/Glyph/Perturb.hs
@@ -1,3 +1,16 @@
+-- Copyright (C) 2016 Michael Smith.
+
+-- Glyph is bi-licensed under the Lesser GNU General Public License Version 3
+-- or later as well as the Mozilla Public License Version 2. You can modify or
+-- redistribute its sources under the conditions of these licenses.
+
+-- The text of the Lesser GNU General Public License Version 3 can found in the
+-- COPYING.LGPL3 file, and can also be obtained from
+-- <http://www.gnu.org/licenses/>.
+
+-- The text of the Mozilla Public License Version 2 can be found in the
+-- COPYING.MPL2 file, and can also be obtained from <http://mozilla.org/MPL/2.0/>.
+
 -- | Randomly perturb source glyphs to produce unique derivative glyphs, to
 -- produce a more natural handwriting feel.
 
